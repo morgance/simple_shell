712bd13f4e87b221f335de9854542b2db3cc32fd
Eodera1 <ericowino8@gmail.com>
2023-05-19 11:55:51 -0700

    Shell file

---
+++ shell.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], 0_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(argv[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(argv[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info[0].readfd = fd;
+	}
+
+	populate_env_list(info);
+	read_memory(info);
+	hsh(info, argv);
+	return (EXIT_SUCCESS);
+}
