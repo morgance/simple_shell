df6ffed1ec2d81e496a5f5fc015fb7644c7e7fc2
morgance <mogeokumu@gmail.com>
2023-05-20 23:40:55 -0700

    updates

--- real_sh.c
+++ real_sh.c
@@ -20,7 +20,7 @@ char *memory_set(char *n, char nm, unsigned int i)
 }
 /**
  * free_sh - free a tring
- * @strng:string 
+ * @strng:string
  */
 void free_sh(char **strng)
 {
@@ -35,4 +35,40 @@ void free_sh(char **strng)
 	}
 	free(strng);
 }
+/**
+ * real_sh - reallocates memory block
+ * @dest:pointer tothe old block of memory
+ * @old_ms:size of the old memory
+ * @new_ms:size of the new memory
+ *
+ * Return:pointer to the new memory
+ */
+void *real_sh(void *dest, unsigned int old_ms, unsigned int new_ms)
+{
+	char *i;
 
+	if (!dest)
+		return (malloc(new_ms));
+	if (!new_ms)
+	{
+		free(dest);
+		return (NULL);
+	}
+
+	if (new_ms == old_ms)
+		return (dest);
+	i = malloc(new_ms);
+	if (!i)
+		return (NULL);
+	/*copy contents from the previous block to the new block*/
+	unsigned int copy_size = old_ms < new_ms ? old_ms : new_ms;
+	char *source = dest;
+	char *destination = i;
+	unsigned int p;
+
+	for (p = 0; p < copy_size; p++)
+		destination[p] = source[p];
+
+	free(dest);
+	return (i);
+}
