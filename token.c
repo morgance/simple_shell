a3c6e6275d107ea9f930da844f80fe6313dd7344
morgance <mogeokumu@gmail.com>
2023-05-23 16:10:43 +0300

    Update token.c

--- token.c
+++ token.c
@@ -64,3 +64,63 @@ char **strtow(char *str, char *delimiters)
 	words[j] = NULL;
 	return (words);
 }
+/**
+ * **token - splits a string into words
+ * @str:input string
+ * @delimeter:delimeter
+ * Return:pointer to array of strings
+ */
+char **token(char *str, char delimiter)
+{
+    int i, j, k, m, word_count = 0;
+    char **words;
+
+    if (str == NULL || str[0] == '\0')
+        return (NULL);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if ((str[i] != delimiter && str[i + 1] == delimiter) ||
+            (str[i] != delimiter && !str[i + 1]) || str[i + 1] == delimiter)
+            word_count++;
+    }
+
+    if (word_count == 0)
+        return (NULL);
+
+    words = malloc((word_count + 1) * sizeof(char *));
+    if (!words)
+        return (NULL);
+
+    i = 0;
+    j = 0;
+
+    while (j < word_count)
+    {
+        while (str[i] == delimiter && str[i] != '\0')
+            i++;
+
+        k = 0;
+        while (str[i + k] != delimiter && str[i + k] != '\0')
+            k++;
+
+        words[j] = malloc((k + 1) * sizeof(char));
+        if (!words[j])
+        {
+            for (k = 0; k < j; k++)
+                free(words[k]);
+            free(words);
+            return (NULL);
+        }
+
+        for (m = 0; m < k; m++)
+            words[j][m] = str[i++];
+
+        words[j][m] = '\0';
+        j++;
+    }
+
+    words[j] = NULL;
+    return (words);
+}
+
