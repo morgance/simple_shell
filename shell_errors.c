135d4b4e1b726dfe992bbbbb6d3c88abaf701689
morgance <mogeokumu@gmail.com>
2023-05-23 02:47:01 -0700

    updates

---
+++ shell_errors.c
@@ -0,0 +1,57 @@
+#include "main.h"
+/**
+ * conv_str_int - converts string to integer
+ * @n:string to be converted
+ *
+ * Return:converted number, -1 otherwise
+ */
+int conv_str_int(char *n)
+{
+	int sign = 1;
+	int j = 0;
+	long int num = 0;
+
+	if (*n == '+')
+		n++;
+	else if (*n == '-')
+	{
+		sign = -1;
+		n++;
+	}
+
+	for (j = 0; n[j] != '\0'; j++)
+	{
+		if (n[j] >= '0' && n[j] <= '9')
+		{
+			num = num * 10 + (n[j] - '0');
+
+			if (sign == 1 && num > INT_MAX)
+				return (-1);
+			else if (sign == -1 && -num < INT_MIN)
+				return (-1);
+		}
+		else
+		{
+			return (-1); /* invalid character, return error */
+		}
+	}
+	return (sign * num);
+}
+/**
+ * print_msg - prints error message
+ * @data:parameter 
+ * @strngs:string with error type
+ *
+ * Return:0 on success, -1 otherwise
+ */
+void print_msg(data_s *data, char strngs)
+{
+	_inputs(data->filename);
+	_inputs(": ");
+	print_deci(data->err_count, STDERR_FILENO);
+	_inputs(": ");
+	_inputs(data->argv[0]);
+	_inputs(": ");
+	_inputs(strngs);
+}
+/**
