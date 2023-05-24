9c394c8782f4a77feb1043d5c28ca5d986dd1718
morgance <mogeokumu@gmail.com>
2023-05-24 18:52:17 +0300

    Update strings_sh.c

--- strings_sh.c
+++ strings_sh.c
@@ -1,3 +1,20 @@
 #include "main.h"
 /**
+ * _strlen - returns length of a string
+ * @str: the string whose length to check
  *
+ * Return: int length of string
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	if (str == NULL)
+		return (0);
+
+	for (; *str != '\0'; str++)
+		length++;
+
+	return (length);
+}
+
