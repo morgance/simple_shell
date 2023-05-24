1baf77a1380f37b58a19a64c08a142e81edd9754
morgance <mogeokumu@gmail.com>
2023-05-24 13:41:32 +0300

    Update builtin_sh.c

--- builtin_sh.c
+++ builtin_sh.c
@@ -10,3 +10,33 @@ int _history(data_s *data)
 	print_elem(data->former_n);
 	return (0);
 }
+/**
+ * unset_alias - unsets alias 
+ * @data: parameter struct
+ * @n:string alias
+ *
+ * Return:0 if succesful, 0 otherwise
+ */
+int unset_alias(data_s *data, char *n)
+{
+    char *i;
+    int pk = 0;
+
+    i = strchr(n, '=');
+    if (!i)
+        return (1);
+
+    *i = '\0';  /* Replace '=' with null terminator to isolate the alias*/
+
+    /* Find the node with the matching alias and delete it*/
+    link_s *node = str_strts(data->alias, n, -1);
+    if (node)
+    {
+        pk = del_node(&(data->alias), get_index(data->alias, node));
+    }
+
+    *i = '=';  /* Restore the original string */
+
+    return (pk);
+}
+
