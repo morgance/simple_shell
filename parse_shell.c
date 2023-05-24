230305f3df521bc56b6500740460f9f8b134713c
morgance <mogeokumu@gmail.com>
2023-05-24 18:35:32 +0300

    Update parse_shell.c

--- parse_shell.c
+++ parse_shell.c
@@ -45,5 +45,54 @@ char *dupli_char(char *pstrn, int sindx, int spinx)
 	pro[j] = '\0';
 	return (pro);
 }
+/**
+ * chk_path - checks cmd in the PATH string
+ * @data:data struct
+ * @pstrn:PATH string
+ * @cmd:cmd to find
+ *
+ * Return:path of cmd 
+ */
+char *chk_path(data_s *data, char *pstrn, char *cmd)
+{
+	int i = 0, dest = 0;
+	char *path;
+
+	if (!pstrn)
+		return (NULL);
+
+	if (_strlen(cmd) > 2 && starts_with(cmd, "./"))
+	{
+		if (is_exec(data, cmd))
+			return (cmd);
+	}
+
+	while (pstrn[i])
+	{
+		if (pstrn[i] == ':')
+		{
+			path = dupli_char(pstrn, dest, i);
+			if (*path)
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			else
+			{
+				path = duplic_str(cmd);
+			}
+
+			if (is_exec(data, path))
+				return (path);
+
+			dest = i + 1;
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
+
 
 
