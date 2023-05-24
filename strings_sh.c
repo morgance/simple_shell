2ed1eeb56871ba764114f606efe88207cb1f075c
morgance <mogeokumu@gmail.com>
2023-05-24 18:56:23 +0300

    Update strings_sh.c

--- strings_sh.c
+++ strings_sh.c
@@ -17,4 +17,25 @@ int _strlen(char *str)
 
 	return (length);
 }
+/**
+ * _strcmp - performs lexicographic comparison of two strings.
+ * @s1:first string
+ * @s2:second string
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
 
