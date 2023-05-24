37e7f5f34067e0270c07bf708e94d84bd3ed0fef
morgance <mogeokumu@gmail.com>
2023-05-24 03:20:46 -0700

    updates

---
+++ builtin_sh.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _history - display history list
+ * @data:structure arguments
+ *
+ * Return:0
+ */
+int _history(data_s *data)
+{
+	print_elem(data->former_n);
+	return (0);
+}
