0b22b037b40232c4e480bcb70fc4372f53b397c8
morgance <mogeokumu@gmail.com>
2023-05-24 17:56:14 +0300

    Update parse_shell.c

--- parse_shell.c
+++ parse_shell.c
@@ -1,3 +1,18 @@
 #include "main.h"
 /**
+ * is_exec - checks if a file is an executable command
+ * @data:data struct
+ * @path:path to the file
  *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_exec(data_s *data, char *path)
+{
+	struct stat st;
+
+	if (path == NULL || stat(path, &st) != 0)
+		return (0);
+
+	return (S_ISREG(st.st_mode));
+}
+
