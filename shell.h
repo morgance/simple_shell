247f8f4e0043e04a8642145e66efec3b6d133296
Eodera1 <ericowino8@gmail.com>
2023-05-27 11:41:27 -0700

    New shell update

--- shell.h
+++ shell.h
@@ -1,29 +1,42 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#include <sys/wait.h>
-#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <string.h>
 #include <sys/types.h>
-#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
 
-#define KEN_WRITE_BS 1024
-#define KEN_BUF_F -1
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
 
-#define KEN_COMMAND_N	0
-#define CMD_OR		1
+extern char **environ;
 
 typedef struct liststr
 {
 	int num;
 	char *str;
 	struct liststr *next;
-} ken_listk_t;
+} ken_list_kt;
+
+#define WRITE_BS 1024
+#define BUF_KFL -1
+#define K_USE_KGETLINE 0
+#define K_USE_KSTRTOK 0
+#define K_CMD_KOR		1
+#define K_CMD_KAND		2
+#define K_CMD_KNORM	0
+#define K_CMD_KCHAIN	3
+#define READ_KBS 1024
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		0, 0, 0}
 
 typedef struct passinfo
 {
@@ -35,141 +48,115 @@ typedef struct passinfo
 	int err_num;
 	int linecount_flag;
 	char *fname;
-	ken_listk_t *env;
-	ken_listk_t *history;
-	ken_listk_t *alias;
+	ken_list_kt *env;
+	ken_list_kt *history;
+	ken_list_kt *alias;
 	char **environ;
 	int env_changed;
 	int status;
 
-	char **cmd_buf;
-	int cmd_buf_type;
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
 	int histcount;
-} info_t;
-
-#define HIST_FILE	".simple_shell_history"
-#define HIST_MAX	4096
-#define CONV_LOW	1
-#define CONV_UNS	2
-#define KEN_READ_BS 1024
-#define CMD_AND		2
-#define CMD_CHAIN	3
-#define USE_GETL 0
-#define USE_STRT 0
-#define INF_INT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-		0, 0, 0}
-
-int hsh(info_t *, char **);
-int f2(int, int);
-int find_bltin(info_t *);
-void *_realloc(void *, unsigned int, unsigned int);
-void fnd_comand(info_t *);
-void forkk_commd(info_t *);
-int f3(int, int);
-char *_strncpy(char *, char *, int);
-char *_strncat(char *, char *, int);
-char **strtow(char *, char *);
-int f4(int, int);
-char **strtow2(char *, char);
-char *_memset(char *, char, unsigned int);
-void ffree(char **);
-int f5(int, int);
-int _strlen(char *);
-int _strcmp(char *, char *);
-int f6(int, int);
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-char *ken_get_khistory_file(info_t *info);
-int f7(int, int);
-int ken_write_khistory(info_t *info);
-int ken_read_khistory(info_t *info);
-int f8(int, int);
-int _myexit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
-
-int is_cmd(info_t *, char *);
-char *dupl_ch(char *, int, int);
-char *find_path(info_t *, char *, char *);
-
-void _eputs(char *);
-int _epucha(char);
-int f1(int, int);
-int ken_ptfd(char c, int fd);
-int ken_psfd(char *str, int fd);
-char *starts_with(const char *, const char *);
-char *_strcat(char *, char *);
-char *_strcpy(char *, char *);
-int f9(int, int);
-char *_strdup(const char *);
-void _puts(char *);
-int _putchar(char);
-ken_listk_t *add_node(ken_listk_t **, const char *, int);
-ken_listk_t *add_node_end(ken_listk_t **, const char *, int);
-int f10(int, int);
-size_t ken_print_klist_str(const ken_listk_t *);
-int ken_delete_knode_at_index(ken_listk_t **, unsigned int);
-void free_list(ken_listk_t **);
-
-int ken_eratoi(char *);
-void print_error(info_t *, char *);
-int f11(int, int);
-int print_d(int, int);
-char *conv_numb(long int, int, int);
-void rem_comments(char *);
-size_t list_len(const ken_listk_t *);
-int f12(int, int);
-
-
-char **ken_listk_to_strings(ken_listk_t *);
-size_t print_list(const ken_listk_t *);
-ken_listk_t *node_starts_with(ken_listk_t *, char *, char);
-int f3(int, int);
-ssize_t get_node_index(ken_listk_t *, ken_listk_t *);
-int _myhistory(info_t *);
-int ken_build_khistory_list(info_t *info, char *buf, int linecount);
-int f14(int, int);
-int ken_renumber_khistory(info_t *info);
-int _myalias(info_t *);
-void clr_info(info_t *);
-int interactive(info_t *);
-int ken_is_dlm(char, char *);
-int f5(int, int);
-int _isalpha(int);
-int _atoi(char *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
-char *_getenv(info_t *, const char *);
-int f6(int, int);
-int _myenv(info_t *);
-int _mysetenv(info_t *);
-int _myunsetenv(info_t *);
-int pop_envlist(info_t *);
-int f7(int, int);
-int bfree(void **);
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void signintHand(int);
-char **ken_get_envron(info_t *);
-int ken__unsektenv(info_t *, char *);
-int ken__setkenv(info_t *, char *, char *);
+} k_info_kt;
 
 typedef struct builtin
 {
 	char *type;
-	int (*func)(info_t *);
-} bltin_tab;
+	int (*func)(k_info_kt *);
+} k_builtin_ktable;
 
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+int k_replace_kvars(k_info_kt *);
+int k_read_khistory(k_info_kt *info);
+int _myexit(k_info_kt *);
+int _mycd(k_info_kt *);
+int _isalpha(int);
+int f1(int, int);
+int _atoi(char *);
+int _myhelp(k_info_kt *);
+ssize_t k_get_kinput(k_info_kt *);
+int f2(int, int);
+int _getline(k_info_kt *, char **, size_t *);
+int _erratoi(char *);
+void k_print_kerror(k_info_kt *, char *);
+int f3(int, int);
+void sigintHandler(int);
+int k_build_khistory_klist(k_info_kt *info, char *buf, int linecount);
+int k_renumber_khistory(k_info_kt *info);
 
-int replace_vars(info_t *);
-int replace_string(char **, char *);
-
-extern char **environ;
-int f8(int, int);
-int shelloop(char **);
+int k_replace_kstring(char **, char *);
+ken_list_kt *add_node(ken_list_kt **, const char *, int);
+ken_list_kt *add_node_end(ken_list_kt **, const char *, int);
+char *_getenv(k_info_kt *, const char *);
+int f4(int, int);
+int _myenv(k_info_kt *);
+int _mysetenv(k_info_kt *);
+int k_print_kd(int, int);
+char *k_convert_knumber(long int, int, int);
+int f5(int, int);
+void k_remove_kcomments(char *);
+int interactive(k_info_kt *);
+int k_is_kdelim(char, char *);
+int _myhistory(k_info_kt *);
+int _myalias(k_info_kt *);
+int k_is_kchain(k_info_kt *, char *, size_t *);
+void k_check_kchain(k_info_kt *, char *, size_t *, size_t, size_t);
+int k_replace_kalias(k_info_kt *);
+size_t k_print_klist_kstr(const ken_list_kt *);
+int delete_knode_at_kindex(ken_list_kt **, unsigned int);
+void k_free_klist(ken_list_kt **);
+void k_find_kcmd(k_info_kt *);
+void k_fork_kcmd(k_info_kt *);
+int _myunsetenv(k_info_kt *);
+void k_free_kinfo(k_info_kt *, int);
+int f6(int, int);
+int populate_kenv_klist(k_info_kt *);
+size_t k_list_klen(const ken_list_kt *);
+char **ken_list_kto_strings(ken_list_kt *);
+int good(int, int);
+int hsh(k_info_kt *, char **);
+int k_find_kbuiltin(k_info_kt *);
+char **k_get_kenviron(k_info_kt *);
+int really(int, int);
+char *get_khistory_kfile(k_info_kt *info);
+int k_write_khistory(k_info_kt *info);
+void k_clear_kinfo(k_info_kt *);
+char *k_starts_kwith(const char *, const char *);
 char *_strchr(char *, char);
+int matt(char, int);
+char *_strcat(char *, char *);char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+void k_set_kinfo(k_info_kt *, char **);
+size_t k_print_klist(const ken_list_kt *);
+char great(int, char *);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+int _strlen(char *);
+int _strcmp(char *, char *);
+ken_list_kt *node_kstarts_kwith(ken_list_kt *, char *, char);
+ssize_t get_knode_kindex(ken_list_kt *, ken_list_kt *);
+int _unsetenv(k_info_kt *, char *);
+int _setenv(k_info_kt *, char *, char *);
+char *_strncpy(char *, char *, int);
+int bfree(void **);
+char *_strncat(char *, char *, int);
+char **strtow(char *, char *);
+char **strtow2(char *, char);
+int is_cmd(k_info_kt *, char *);
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+char *dup_chars(char *, int, int);
+void _eputs(char *);
+int _eputchar(char);
+void _puts(char *);
+int _putchar(char);
+char *k_find_kpath(k_info_kt *, char *, char *);
+int loophsh(char **);
 
 #endif
