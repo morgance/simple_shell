ff8e11e133bb2c3c8d52085342ab4cbdbece0b31
morgance <mogeokumu@gmail.com>
2023-05-27 10:33:56 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,190 +1,175 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <limits.h>
 #include <sys/wait.h>
-#include <stdbool.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
 #include <errno.h>
 
-#define READ_BUFF 1024
-#define WRITE_BUFF 1024
-#define FLUSH_BUFF -1
-#define LOWER_CON 1
-#define C_UNSIGHNED 2
-#define CMD_NM 0
-#define CMD_O 1
-#define CMD_A 2
-#define CMD_C 3
-#define GETLINE_SH 0
-#define STRTOK_SH 0
-#define PST_FILE "shell_history"
-#define FILE_PA 4096
+#define KEN_WRITE_BS 1024
+#define KEN_BUF_F -1
+
+#define KEN_COMMAND_N	0
+#define CMD_OR		1
+
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} ken_listk_t;
+
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	ken_listk_t *env;
+	ken_listk_t *history;
+	ken_listk_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf;
+	int cmd_buf_type;
+	int readfd;
+	int histcount;
+} info_t;
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+#define CONV_LOW	1
+#define CONV_UNS	2
+#define KEN_READ_BS 1024
+#define CMD_AND		2
+#define CMD_CHAIN	3
+#define USE_GETL 0
+#define USE_STRT 0
+#define INF_INT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		0, 0, 0}
+
+int hsh(info_t *, char **);
+int f2(int, int);
+int find_bltin(info_t *);
+void *_realloc(void *, unsigned int, unsigned int);
+void fnd_comand(info_t *);
+void forkk_commd(info_t *);
+int f3(int, int);
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char **strtow(char *, char *);
+int f4(int, int);
+char **strtow2(char *, char);
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+int f5(int, int);
+int _strlen(char *);
+int _strcmp(char *, char *);
+int f6(int, int);
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+char *ken_get_khistory_file(info_t *info);
+int f7(int, int);
+int ken_write_khistory(info_t *info);
+int ken_read_khistory(info_t *info);
+int f8(int, int);
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+int is_cmd(info_t *, char *);
+char *dupl_ch(char *, int, int);
+char *find_path(info_t *, char *, char *);
+
+void _eputs(char *);
+int _epucha(char);
+int f1(int, int);
+int ken_ptfd(char c, int fd);
+int ken_psfd(char *str, int fd);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+char *_strcpy(char *, char *);
+int f9(int, int);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+ken_listk_t *add_node(ken_listk_t **, const char *, int);
+ken_listk_t *add_node_end(ken_listk_t **, const char *, int);
+int f10(int, int);
+size_t ken_print_klist_str(const ken_listk_t *);
+int ken_delete_knode_at_index(ken_listk_t **, unsigned int);
+void free_list(ken_listk_t **);
+
+int ken_eratoi(char *);
+void print_error(info_t *, char *);
+int f11(int, int);
+int print_d(int, int);
+char *conv_numb(long int, int, int);
+void rem_comments(char *);
+size_t list_len(const ken_listk_t *);
+int f12(int, int);
+
+
+char **ken_listk_to_strings(ken_listk_t *);
+size_t print_list(const ken_listk_t *);
+ken_listk_t *node_starts_with(ken_listk_t *, char *, char);
+int f3(int, int);
+ssize_t get_node_index(ken_listk_t *, ken_listk_t *);
+int _myhistory(info_t *);
+int ken_build_khistory_list(info_t *info, char *buf, int linecount);
+int f14(int, int);
+int ken_renumber_khistory(info_t *info);
+int _myalias(info_t *);
+void clr_info(info_t *);
+int interactive(info_t *);
+int ken_is_dlm(char, char *);
+int f5(int, int);
+int _isalpha(int);
+int _atoi(char *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+char *_getenv(info_t *, const char *);
+int f6(int, int);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int pop_envlist(info_t *);
+int f7(int, int);
+int bfree(void **);
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void signintHand(int);
+char **ken_get_envron(info_t *);
+int ken__unsektenv(info_t *, char *);
+int ken__setkenv(info_t *, char *, char *);
+
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} bltin_tab;
+
+
+int replace_vars(info_t *);
+int replace_string(char **, char *);
 
 extern char **environ;
-
-/**
- * struct linkedlst - linled list
- * @ber:the number
- * @srt:path
- * @next:the next node pointer
- */
-typedef struct linkedlst
-{
-	int ber;
-	char *srt;
-	struct linkedlst *next;
-} link_s;
-/**
- * struct datainfo - pseudo-arguments that will be passed into a function
- * @path:path for command
- * @str:string containing arguments
- * @argc:argument count
- * @argv:array of strings
- * @err_exits:error for exit
- * @err_count:count error
- * @_envi_:modified environ
- * @former_n:history node
- * @counter:line of input count
- * @filename:filename of the program
- * @alias:alias node
- * @status:status of the last command
- * @cmd_add:adress of pointer
- * @cmd_ty:cmd type
- * @read_in:read line input
- * @_env:linked list
- * @env_new:changed environ
- * @past_line:number line history
- */
-typedef struct datainfo
-{
-	char *path;
-	char filename;
-	char **argv;
-	char **_envi_;
-	char **cmd_add;
-	char *str;
-	int cmd_ty;
-	int env_new;
-	int read_in;
-	int counter;
-	int argc;
-	int err_exits;
-	int status;
-	int past_line;
-	link_s *alias;
-	link_s *former_n;
-	link_s *_env;
-	unsigned int err_count;
-} data_s;
-
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
-
-/**
- * struct built - builtin string
- * @comm:builtin command
- * @func:function
- */
-typedef struct built
-{
-	char *comm;
-	int (*func)(data_s *);
-} built_t;
-
-
-
-int _history(data_s *data);
-int unset_alias(data_s *data, char *n);
-int set_alias(data_s *data, char *n);
-int print_alias_str(link_s *pro);
-int alias_sh(data_s *data);
-
-void data_info(data_s *data);
-void set_data(data_s *data, char **ph);
-void free_strct(data_s *data, int pst);
-void _inputs(char strg);
-int _charac(char n);
-int _wrfd(char n, int filed);
-
-int _prnt(char *stng, int filed);
-char *memory_set(char *n, char nm, unsigned int i);
-void free_sh(char **strng);
-void *real_sh(void *dest, unsigned int old_ms, unsigned int new_ms);
-int conv_str_int(char *n);
-
-void print_msg(data_s *data, char strngs);
-int print_deci(int input, int filed);
-void remove_comms(char *addr);
-char *_numcon(long int num, int base, int flags);
-link_s *node_new(link_s **hd, const char *srt, int ber);
-link_s *node_end(link_s **hd, const char *srt, int ber);
-size_t print_str(const link_s *head);
-
-int del_node(link_s **dest, unsigned int deleto);
-void free_nodes(link_s **head);
-size_t leng_ls(const link_s *head);
-char **arry_strng(link_s *head);
-size_t print_elem(const int *head);
-link_s *str_strts(link_s *head, const char pfix, char n);
-ssize_t get_index(link_s *head, link_s *pnodes);
-char *copy_str(char *prim, const char frm);
-
-char *duplic_str(const char *strng);
-void print_inp(const char *strng);
-int _putchar(char c);
-char **strtow(char *str, char *delimiters);
-char **token(char *str, char delimiter);
-int test_deli(data_s *data, char *chbf, size_t *ap);
-void st_check(data_s *data, char *chbf, size_t leng, size_t aj, size_t *ap);
-
-int alias_rep(data_s *data);
-int vars_str(data_s *data);
-int _string_rp(char **old_str, char *new_str);
-int collective(data_s *data);
-
-int _isalpha(int c);
-int _atoi(char *s);
-int is_lim(char c, char *lim);
-void data_info(data_s *data);
-void set_data(data_s *data, char **ph);
-void free_strct(data_s *data, int pst);
-
-int set_environment_variable(char **env, const char *name, const char *value);
-int exit_handle(data_s *data);
-int mkdir_process(data_s *data);
-char *_strprint(char *dest, const char *src, int n);
-char *_strcpy(char *dest, char *src, int n);
-char **get_env(data_s *data);
-int _getenv(data_s *data, char *var, char *value);
-int my_environ(data_s *data);
-
-char *exits_env(data_s *data, const char *name);
-int print_lists_env(data_s *data);
-int _erase_env(data_s *data);
-int hsh(data_s *data, char **argv);
-int find_path(data_s *data);
-void find_error(data_s *data);
-
-int main(int argc, char **argv);
-int bfree(void **ptr);
-char *get_string_file(data_s *data);
-int write_string(data_s *data);
-int read_string(data_s *data);
-int is_exec(data_s *data, char *path);
-char *dupli_char(char *pstrn, int sindx, int spinx);
-char *chk_path(data_s *data, char *pstrn, char *cmd);
-int _strlen(char *str);
-
-int _strcmp(char *s1, char *s2);
-char *starts_with(const char *haystack, const char *needle);
-char *_strcat(char *dest, const char *src);
+int f8(int, int);
+int shelloop(char **);
+char *_strchr(char *, char);
 
 #endif
